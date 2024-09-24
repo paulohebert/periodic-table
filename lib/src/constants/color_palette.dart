a2fff624f2a341a5fb49de2c27ad513ac39b78a5
paulohebert <paulo.hebert@discente.univasf.edu.br>
2024-09-24 20:38:48 -0300

    feat: add color palette

---
+++ lib/src/constants/color_palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'element_category.dart';
+
+const Map<ElementCategory, Color> colors = {
+  ElementCategory.metal: Color.fromRGBO(75, 209, 104, 1),
+  ElementCategory.nonmetal: Color.fromRGBO(240, 112, 20, 1),
+  ElementCategory.semimetal: Color.fromRGBO(240, 178, 20, 1),
+};
