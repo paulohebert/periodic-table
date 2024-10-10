4611fc9fa4636e3fde3d5b0e56a85d65f02c9af0
vinycios10 <vinycios1028@gmail.com>
2024-10-10 09:46:12 -0300

    add legenda

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -42,7 +42,75 @@ class HomeScreen extends StatelessWidget {
               onTap: () => FocusScope.of(context).unfocus(),
               child: const PeriodicTableWidget())),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                height: 200,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text(
+                        'Grupos',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            color: Color(0xFF4BD169), 
+                          ),
+                          const SizedBox(width: 8.0),
+                          const Text('Metais'),
+                        ],
+                      ),
+                      const SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            color: Color(0xFFF0B214), 
+                          ),
+                          const SizedBox(width: 8.0),
+                          const Text('Semimetais'),
+                        ],
+                      ),
+                      const SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            color: Color(0xFFF07014),
+                          ),
+                          const SizedBox(width: 8.0),
+                          const Text('Não-metais'),
+                        ],
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        child: const Text('Close BottomSheet'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
         tooltip: 'Informações',
         shape: const CircleBorder(),
         child: const Icon(Icons.info_outline),
