0ca032187bc82331230a03d0b4331e732aadd971
paulohebert <paulo.hebert@discente.univasf.edu.br>
2024-11-15 15:26:05 -0300

    fix: bug in 3d viewer

--- lib/src/widgets/element_info.dart
+++ lib/src/widgets/element_info.dart
@@ -3,15 +3,16 @@ import 'package:model_viewer_plus/model_viewer_plus.dart';
 import '../models/element_details.dart';
 
 class ElementInfo extends StatelessWidget {
-  const ElementInfo({super.key, required this.data});
+  ElementInfo({super.key, required this.data});
 
   final ChemicalElementDetails data;
+
+  final GlobalKey modelViewerKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     final bool isDarkTheme = Theme.of(context).brightness == Brightness.dark;
-    return SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
       Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
         child: Text(
@@ -19,21 +20,20 @@ class ElementInfo extends StatelessWidget {
           softWrap: true,
         ),
       ),
-      SizedBox(
-          width: MediaQuery.sizeOf(context).width,
-          height: MediaQuery.sizeOf(context).height,
+      Expanded(
           child: ModelViewer(
-            backgroundColor:
-                isDarkTheme ? const Color(0xFF303030) : const Color(0xFFEEEEEE),
-            src: 'assets/assets/model3d/${data.symbol}.glb',
-            alt: 'Átomo de ${data.name}',
-            ar: true,
-            autoRotate: true,
-            shadowIntensity: 1,
-            autoPlay: true,
-            disableZoom: true,
-            debugLogging: false,
-          ))
-    ]));
+        key: modelViewerKey,
+        backgroundColor:
+            isDarkTheme ? const Color(0xFF303030) : const Color(0xFFEEEEEE),
+        src: 'assets/assets/model3d/${data.symbol}.glb',
+        alt: 'Átomo de ${data.name}',
+        ar: true,
+        autoRotate: true,
+        shadowIntensity: 1,
+        autoPlay: true,
+        disableZoom: true,
+        debugLogging: false,
+      ))
+    ]);
   }
 }
